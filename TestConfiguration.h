cb4fd350b72f1bde3eb0c98ea723a3224a91ad6f
Anders M <AndersM91@hotmail.dk>
2017-12-14 16:17:01 +0100

    -Reuse command buffer is now NOT standard

--- TestConfiguration.h
+++ TestConfiguration.h
@@ -5,7 +5,7 @@
 
 struct TestConfiguration
 {
-	bool reuseCommandBuffers = true;
+	bool reuseCommandBuffers = false;
 	size_t probeInterval = 0;	//make probes every frame. Interval is given in ms.
 	size_t seconds = 0;	//run until program is closed by user
 	bool exportCsv = false;
