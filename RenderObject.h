c420ed5c85c1b2e0b98e256a4140813204a2daf1
Anders M <AndersM91@hotmail.dk>
2017-12-21 20:50:20 +0100

    - rotation angle added to render-objects (only used by Vulkan)

--- RenderObject.h
+++ RenderObject.h
@@ -1,9 +1,10 @@
 #pragma once
-
 class RenderObject
 {
 public:
 	explicit RenderObject(float x, float y, float z) : m_X(x), m_Y(y), m_Z(z) { }
+
+	int m_RotationAngle;
 
 	float x() const { return m_X; }
 	float y() const { return m_Y; }
